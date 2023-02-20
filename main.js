715d7e30d74892da3bb93ce73581a148ad93bce9
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-02-20 21:47:02 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { DateTime } from './node_modules/luxon/src/luxon.js';
+import { DateTime } from './modules/luxon.js';
 
 import {
   pageOne, pageTwo, pageThree, listLink, addLink, contactLink,
